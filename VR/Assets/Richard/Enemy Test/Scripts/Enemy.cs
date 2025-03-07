5f46ec5c430b22587e2ec3077ab14a31ac797b6f
Richardbertan <Richardbertan18@gmail.com>
2025-03-07 14:08:32 -0300

    Inimigo, pastas organizadas
    Inimigo consegue detectar o player mais proximo

--- VR/Assets/Richard/Enemy Test/Scripts/Enemy.cs
+++ VR/Assets/Richard/Enemy Test/Scripts/Enemy.cs
@@ -1,27 +1,32 @@
+using System;
 using System.Collections;
 using System.Linq;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField]Transform[] Players = new Transform[4];
+    [SerializeField] Transform[] Players = new Transform[4]; // array de players
 
     void Start()
     {
-        StartCoroutine(FindClosest());
+        FindClosestPlayer();
     }
 
     IEnumerator FindClosest()
     {
-        float[] playerDistances = new float[4];
+        float[] playerDistances = new float[4]; // array de distancias
+
         for (int i = 0; i < Players.Length; i++)
         {
             playerDistances[i] = Vector3.Distance(Players[i].transform.position, transform.position);
-            //print(playerDistances[i] + $", {i}");
         }
-        print(playerDistances.Min());
+        print(Players[Array.IndexOf(playerDistances, playerDistances.Min())]); // Coleta o index do array baseado no menor distancia
         yield return new WaitForSeconds(2.5f);
         StartCoroutine(FindClosest());
     }
 
+    void FindClosestPlayer()
+    {
+        StartCoroutine(FindClosest());
+    }
 }
---
+++ VR/Assets/Richard/Enemy Test/Scripts/Flying Enemy.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class FlyingEnemy : Enemy
+{
+    private void Start()
+    {
+        //FindClosestPlayer();
+    }
+}
