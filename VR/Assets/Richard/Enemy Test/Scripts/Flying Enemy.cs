0656867415c3a55aeabc2a1b5eab038c95cf450c
Richardbertan <Richardbertan18@gmail.com>
2025-03-07 16:38:03 -0300

    Enemy Almost done
    Ele consegue indentificar o mais proximo a ele e pega o transform

--- VR/Assets/Richard/Enemy Test/Scripts/Flying Enemy.cs
+++ VR/Assets/Richard/Enemy Test/Scripts/Flying Enemy.cs
@@ -5,7 +5,6 @@ using System.Collections;
 
 public class FlyingEnemy : Enemy
 {
-    RaycastHit hit;
     protected override IEnumerator FindClose(Transform[] players)
     {
         float[] playerDistances = new float[4]; // array de distancias
@@ -16,23 +15,6 @@ public class FlyingEnemy : Enemy
         }
         followingPlayer = players[Array.IndexOf(playerDistances, playerDistances.Min())]; // Coleta o index do array baseado no menor distancia
         print(followingPlayer.name + ", Aerial");
-
-        if (Physics.Linecast(transform.position, followingPlayer.position, out hit))
-        {
-            
-            if (hit.collider.tag == "Player")
-            {
-                print("cumpensa");
-                print(hit.collider.gameObject.name);
-            }
-            else
-            {
-                float[] TempDistances = playerDistances;
-                print("num cumpensa");
-                followingPlayer = players[Array.IndexOf(playerDistances, playerDistances.Min())];
-            }
-        }
-        
         yield return new WaitForSeconds(1f);
         StartCoroutine(FindClose(players));
     }
