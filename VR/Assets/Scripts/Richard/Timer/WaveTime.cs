798a8a7b61fb46258d59281f8be3324c603412a9
Richardbertan <Richardbertan18@gmail.com>
2025-04-11 15:24:55 -0300

    timer testing

--- VR/Assets/Scripts/Richard/Timer/WaveTime.cs
+++ VR/Assets/Scripts/Richard/Timer/WaveTime.cs
@@ -13,79 +13,30 @@ public class WaveTime : MonoBehaviourPunCallbacks
     [SerializeField] int startTime;
     [SerializeField] float countdown;
 
-    public UnityEvent OnStartTime;
     [SerializeField] List<TextMeshProUGUI> timerText;
-    List<GameObject> players = new List<GameObject>();
 
-
-    private void Awake()
+    public void Start()
     {
-        foreach (GameObject player in players)
-        {
-            timerText.Add(player.GetComponentInChildren<TextMeshProUGUI>());
-            print(player);
-        }
         countdown = startTime;
-        OnStartTime.AddListener(delegate
-        {
-            StartCoroutine(PrintTime());
-        });
     }
-
     public void StartTime()
     {
-        OnStartTime.Invoke();
-    }
-
-    private float TimeRemaining()
-    {
-        int timer = PhotonNetwork.ServerTimestamp - this.startTime;
-        return this.countdown - timer / 1000f;
+        if (PhotonNetwork.IsMasterClient)
+        {
+            StartCoroutine(PrintTime());
+        }
     }
 
     IEnumerator PrintTime()
     {
-        yield return new WaitForSeconds(1);
-        TimeRemaining();
-        foreach (var text in timerText)
-        {
-            text.text = TimeRemaining().ToString();
-            print(text);
-        }
-        print(countdown);
+        yield return new WaitForSeconds(0);
+        countdown -= Time.deltaTime;
+        print((int)countdown);
         if (countdown <= 0)
         {
             print("Cabo");
+            yield return null;
         }
+        StartCoroutine(PrintTime());
     }
-    /*
-    public static bool TryGetStartTime(out int startTimestamp)
-    {
-        startTimestamp = PhotonNetwork.ServerTimestamp;
-
-        object startTimeFromProps;
-        if (PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue(CountdownStartTime, out startTimeFromProps))
-        {
-            startTimestamp = (int)startTimeFromProps;
-            return true;
-        }
-
-        return false;
-    }
-
-
-    public static void SetStartTime()
-    {
-        int startTime = 0;
-        bool wasSet = TryGetStartTime(out startTime);
-
-        Hashtable props = new Hashtable
-            {
-                {CountdownTimer.CountdownStartTime, (int)PhotonNetwork.ServerTimestamp}
-            };
-        PhotonNetwork.CurrentRoom.SetCustomProperties(props);
-
-
-        Debug.Log("Set Custom Props for Time: " + props.ToStringFull() + " wasSet: " + wasSet);
-    }*/
 }
