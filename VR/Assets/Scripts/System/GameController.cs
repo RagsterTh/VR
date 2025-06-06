9546071173bdc9409b716b1c264376fd41b114ad
RagsterTh <139650575+RagsterTh@users.noreply.github.com>
2025-06-06 11:13:43 -0300

    Ultima correção

--- VR/Assets/Scripts/System/GameController.cs
+++ VR/Assets/Scripts/System/GameController.cs
@@ -64,9 +64,9 @@ public class GameController : MonoBehaviour
         PhotonNetwork.CurrentRoom.IsOpen = false;
         _phView.RPC("RPC_BattleBegin", RpcTarget.All);
     }
-    public void RefreshPlayerList()
+    public void RemovePlayerAvatar(int playerID)
     {
-        _phView.RPC("RPC_RefreshPlayerList", RpcTarget.All);
+        _phView.RPC("RPC_RemovePlayerAvatar", RpcTarget.All, playerID);
     }
 
     //RPC's
@@ -76,16 +76,16 @@ public class GameController : MonoBehaviour
         _playerAvatar.Add(PhotonNetwork.GetPhotonView(playerID).transform.GetChild(1).gameObject);
     }
     [PunRPC]
-    public void RPC_RefreshPlayerList()
-    {//Precisa ser modificado ainda
-        for (int i = 0; i < _playerAvatar.Count; i++)
+    public void RPC_RemovePlayerAvatar(int playerID)
+    {
+        foreach (var player in _playerAvatar)
         {
-            if (_playerAvatar[i] == null)
+            if(player.GetPhotonView().ViewID == playerID)
             {
-                _playerAvatar.RemoveAt(i);
+                PhotonNetwork.Destroy(player);
+                _playerAvatar.Remove(player);
                 break;
             }
-
         }
         OnPlayerLeftBattle.Invoke();
     }
