383ef61c603ff63259bc47948ce93d2caee38fae
Gustavo de Medeiros <gustavormedeiros2008@gmail.com>
2025-06-13 14:42:03 -0300

    SegueCamera

--- VR/Assets/Scripts/Julio/Medical questions/Canvas/RotateCanvas.cs
+++ VR/Assets/Scripts/Julio/Medical questions/Canvas/RotateCanvas.cs
@@ -2,13 +2,25 @@ using UnityEngine;
 
 public class RotateCanvas : MonoBehaviour
 {
-    [SerializeField] GameObject rotationBase;
-    [SerializeField] GameObject objToRotate;
+    [SerializeField] Transform cameraTransform;
+    [SerializeField] Transform hudObject;
+    [SerializeField] float distanceAhead = 2f;
+    [SerializeField] float heightOffset = 0f;
+    [SerializeField] float positionSmooth = 5f;
+    [SerializeField] float rotationSmooth = 5f;
 
-    private void Update()
+    void Update()
     {
-        Vector3 currentRotation = objToRotate.transform.eulerAngles;
-        float targetY = rotationBase.transform.eulerAngles.y;
-        objToRotate.transform.eulerAngles = new Vector3(currentRotation.x, targetY, currentRotation.z);
+        if (cameraTransform == null || hudObject == null) return;
+
+        Vector3 forward = cameraTransform.forward;
+        forward.y = 0;
+        forward.Normalize();
+
+        Vector3 targetPos = cameraTransform.position + forward * distanceAhead + Vector3.up * heightOffset;
+        hudObject.position = Vector3.Lerp(hudObject.position, targetPos, Time.deltaTime * positionSmooth);
+
+        Quaternion targetRot = Quaternion.LookRotation(hudObject.position - cameraTransform.position);
+        hudObject.rotation = Quaternion.Slerp(hudObject.rotation, targetRot, Time.deltaTime * rotationSmooth);
     }
 }
