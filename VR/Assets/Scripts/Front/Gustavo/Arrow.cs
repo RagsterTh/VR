36c032441401c3cc17faa630bf7509817b562f8b
Bruno Martins <brunomartins616@hotmail.com>
2025-06-13 15:29:38 -0300

    Seta mehorada

--- VR/Assets/Scripts/Front/Gustavo/Arrow.cs
+++ VR/Assets/Scripts/Front/Gustavo/Arrow.cs
@@ -4,12 +4,20 @@ public class Arrow : MonoBehaviour
 {
     public float speed = 2f; // Velocidade do movimento
     public float distance = 3f; // Distância máxima do movimento
+    private Vector3 startPos; // Posição inicial local do objeto
+
+    void Start()
+    {
+        // Armazena a posição inicial local do objeto
+        startPos = transform.position;
+    }
 
     // Update é chamado uma vez por frame
     void Update()
     {
         // Calcula o movimento usando Mathf.PingPong
         float movement = Mathf.PingPong(Time.time * speed, distance);
-        transform.position = new Vector3(movement, transform.position.y, transform.position.z);
+        // Movimento relativo à rotação local do objeto
+        transform.position = startPos + transform.right * movement;
     }
 }
