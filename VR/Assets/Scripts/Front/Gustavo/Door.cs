21b37f9b8b5c33d16cf080ee7dcfb0a108fe202d
Bruneca <brunomartins616@hotmail.com>
2025-06-22 19:23:17 -0300

    att DoorAnimation

--- VR/Assets/Scripts/Front/Gustavo/Door.cs
+++ VR/Assets/Scripts/Front/Gustavo/Door.cs
@@ -3,57 +3,31 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] private GameObject _door1;
-    [SerializeField] private GameObject _door2;
-
-    private Vector3 _door1Closed;
-    private Vector3 _door1Opened;
-    private Vector3 _door2Closed;
-    private Vector3 _door2Opened;
-    [SerializeField] private float distance;
+    [SerializeField] private string _animationName;
+    private Animation _animation;
 
 
     void Start()
     {
-        _door1Closed = _door1.transform.localPosition;
-        _door2Closed = _door2.transform.localPosition;
-        _door1Opened = _door1.transform.localPosition;
-        _door2Opened = _door2.transform.localPosition;
-        _door1Opened.z = 0;
-        _door2Opened.z = 0;
+        _animation = GetComponent<Animation>();
+        if (_animation == null)
+        {
+            Debug.LogError("Door: No Animation component found on this GameObject.");
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Collider col = GetComponent<Collider>();
-
-            StartCoroutine(InterpolatePos(_door1Closed, _door1Opened, 1f, _door1, true));
-            StartCoroutine(InterpolatePos(_door2Closed, _door2Opened, 1f, _door2, false));
+            if (_animation != null && !string.IsNullOrEmpty(_animationName))
+            {
+                _animation.Play(_animationName);
+                _animation.wrapMode = WrapMode.Once;
+                Debug.Log("Door: Playing animation " + _animationName);
+            }
             col.enabled = false;
         }
     }
-
-    // IEnumerator InterpolatePos(Vector3 start, Vector3 end, float time, GameObject obj)
-    // {
-    //     float elapsedTime = 0f;
-    //     while (elapsedTime < time)
-    //     {
-    //         obj.transform.localPosition = Vector3.Lerp(start, end, elapsedTime / time);
-    //         elapsedTime += Time.deltaTime;
-    //         yield return null;
-    //     }
-    //     obj.transform.localPosition = end;
-    // }
-    IEnumerator InterpolatePos(Vector3 start, Vector3 end, float time, GameObject obj, bool toLeft){
-        float elapsedTime = 0f;
-        float zindex;
-        if (toLeft) zindex = -1; else zindex = 1;
-        while (elapsedTime < time)
-        {
-            obj.transform.localPosition = Vector3.Lerp(start, new Vector3(start.x, end.y, start.z-zindex*distance ), elapsedTime / time);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-    }
+   
 }
