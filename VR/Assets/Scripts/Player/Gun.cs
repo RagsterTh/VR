95f7c5d70ad8c2d7792576b302a1f69b33395083
Bruneca <brunomartins616@hotmail.com>
2025-07-03 23:44:33 -0300

    gunsound

--- VR/Assets/Scripts/Player/Gun.cs
+++ VR/Assets/Scripts/Player/Gun.cs
@@ -8,15 +8,17 @@ public class Gun : MonoBehaviour
     [SerializeField] float _bulletSpeed = 4;
     ObjectPool _playersBullets;
     Transform _gunPoint;
+    ISoundable _soundEmitter;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _phView = GetComponentInParent<PhotonView>();
-        if(!_phView.IsMine)
+        if (!_phView.IsMine)
             enabled = false;
 
         _gunPoint = transform.GetChild(0);
         _playersBullets = GameController.instance.PlayersBullets;
+        _soundEmitter = GetComponent<ISoundable>();
     }
 
     // Update is called once per frame
@@ -29,6 +31,7 @@ public class Gun : MonoBehaviour
         if (value.performed)
         {
             _playersBullets.CallObject(_gunPoint.position, _gunPoint.rotation);
+            _soundEmitter.PlaySound();
             //temp.GetComponent<Rigidbody>().linearVelocity = -transform.up * _bulletSpeed;
         }
 
