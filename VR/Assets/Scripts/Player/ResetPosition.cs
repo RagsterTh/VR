700ce28f85d1d2bfbfc479e262eb955590958459
RagsterTh <139650575+RagsterTh@users.noreply.github.com>
2025-08-22 15:54:58 -0300

    Ultimas correções

--- VR/Assets/Scripts/Player/ResetPosition.cs
+++ VR/Assets/Scripts/Player/ResetPosition.cs
@@ -1,8 +1,10 @@
 using Meta.XR.Editor.Id;
 using System.Collections;
+using System.Collections.Generic;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.InputSystem.XR;
+using UnityEngine.XR;
 
 public class ResetPosition : MonoBehaviour
 {
@@ -14,8 +16,14 @@ public class ResetPosition : MonoBehaviour
         _origin = GetComponent<XROrigin>();
         _playerNetwork = GetComponentInParent<PlayerPrefabNetwork>();
     }
-    private void Start()
+    private IEnumerator Start()
     {
+        yield return new WaitForSeconds(3);
+        var subsystems = new List<XRInputSubsystem>();
+        SubsystemManager.GetSubsystems(subsystems);
+
+        foreach (var s in subsystems)
+            s.TryRecenter(); // pede ao runtime para recentrar o HMD
         _origin.MoveCameraToWorldLocation(Vector3.zero);
         //_origin.Camera.transform.position = Vector3.zero;
     }
