66f5ca1fcf41125c3c68f62b6196fbe05ae74339
RagsterTh <139650575+RagsterTh@users.noreply.github.com>
2025-08-22 14:33:54 -0300

    Testando mapeamento de posição

--- VR/Assets/Scripts/Player/ResetPosition.cs
+++ VR/Assets/Scripts/Player/ResetPosition.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 public class ResetPosition : MonoBehaviour
 {
+    TrackedPoseDriver trackingVR;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        transform.position = Vector3.zero;
+        trackingVR = GetComponent<TrackedPoseDriver>();
+        trackingVR.trackingType = TrackedPoseDriver.TrackingType.RotationOnly;
+        //transform.position = Vector3.zero;
+    }
+    private void Start()
+    {
+        trackingVR.trackingType = TrackedPoseDriver.TrackingType.RotationAndPosition;
     }
 
     // Update is called once per frame
