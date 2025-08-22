00e2ad6e2aa8a8fb2e3031493f3c3870a5a88f88
RagsterTh <139650575+RagsterTh@users.noreply.github.com>
2025-08-22 14:25:33 -0300

    Correção do posicionamento

---
+++ VR/Assets/Scripts/Player/ResetPosition.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
